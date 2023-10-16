129308729bce8ccd5c2b05caffb5c8d6847e1fe2
Aleksandra <aleksandrav@inbox.ru>
2023-10-15 19:45:22 -0700

    Task 2 added

---
+++ Task_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+char vtoraya(string chislo, int num_of_letter)
+{
+    if (chislo.Length >= num_of_letter)
+    {
+        return chislo[num_of_letter-1];
+    }
+    else
+    {
+    Console.Write($"{num_of_letter}-й элемент отсутсвует");
+    }
+    return '!';
+}
+Console.WriteLine(vtoraya(Console.ReadLine(),Convert.ToInt32(Console.ReadLine())));
---
+++ Task_3/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+int num = Convert.ToInt32(Console.ReadLine());
+if(num>5 && num<8){
+    Console.WriteLine("Да");
+}
+else if (num>0 && num<=5)
+{
+    Console.WriteLine("Нет");
+}
+else{
+    Console.WriteLine("Введено не правильное число");
+}
