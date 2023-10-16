df243e453ec5988b646291155184d9cb5390c320
Aleksandra <aleksandrav@inbox.ru>
2023-10-15 19:10:02 -0700

    Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+int show_second_number(int iskomoe){
+    int result = (iskomoe/10)%10;
+    return result;
+}
+
+Console.WriteLine ("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(show_second_number(number));
